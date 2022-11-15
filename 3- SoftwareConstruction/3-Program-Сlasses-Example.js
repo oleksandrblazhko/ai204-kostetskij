2a1e1ba6aea7f785703abd7a59022e401dd9ecc2
Bogdan Kostetskiy <79446121+ninjiaga@users.noreply.github.com>
2022-11-15 18:40:32 +0200

    Create 3-Program-Сlasses-Example.js

---
+++ 3- SoftwareConstruction/3-Program-Сlasses-Example.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const Worker = new Schema({
+    user : Worker.id,
+    email: String,
+    name: String
+});
+
+const Admin = new Schema({
+    id: int
+});
+
+const Sign = new Schema({
+    id: int,
+    user: Worker.id,
+    date: String,
+    duration: int,
+    comment: String
+})
+
+const Document = new Schema({
+    id: int,
+    comment: String
+})
+
+module.export = Worker, Admin, Sign, Document
