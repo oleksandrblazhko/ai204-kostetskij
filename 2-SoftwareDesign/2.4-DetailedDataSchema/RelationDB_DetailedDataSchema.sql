be7326534cacc017e47f6588b64aeac3799d516e
Bogdan Kostetskiy <79446121+ninjiaga@users.noreply.github.com>
2022-11-24 11:17:36 +0200

    Update RelationDB_DetailedDataSchema.sql

--- 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
+++ 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
@@ -14,28 +14,27 @@ CREATE TABLE workers(
 	admin_id INT REFERENCES admin(admin_id),
 	first_name VARCHAR CHECK(char_length(first_name) BETWEEN 2 AND 30 ),
 	surname VARCHAR CHECK(char_length(surname) BETWEEN 2 AND 30),
-  date_of_birth DATE
+        date_of_birth DATE,
 	position VARCHAR,
-  mobile VARCHAR CHECK (mobile ~* '^\+?3?8?(0[\s\.-]\d{2}[\s\.-]\d{3}[\s\.-]\d{2}[\s\.-]\d{2})$')
-	email VARCHAR CHECK(email ~* '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$'),
+        mobile VARCHAR CHECK (mobile ~* '^\+?3?8?(0[\s\.-]\d{2}[\s\.-]\d{3}[\s\.-]\d{2}[\s\.-]\d{2})$'),
+	email VARCHAR CHECK(email ~* '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
 );
 
 CREATE TABLE signature_form(
 	signature_form INT PRIMARY KEY,
 	signature VARCHAR,
-  comment VARCHAR CHECK(char_length(locations_list) <= 500)
-  
+        comment VARCHAR CHECK(char_length(locations_list) <= 500)
 );
 
 CREATE TABLE safety_doc(
-  safety_doc_id INT PRIMARY KEY,
+        safety_doc_id INT PRIMARY KEY,
 	document VARCHAR
 );
 
 CREATE TABLE online_message ( 
 	onlineMessage_id INT PRIMARY KEY,
-  workers_id INT REFERENCES workers(workers_id),
-  admin_id INT REFERENCES admin(admin_id),
+        workers_id INT REFERENCES workers(workers_id),
+        admin_id INT REFERENCES admin(admin_id),
 	email VARCHAR,  
 	text VARCHAR 
 );
